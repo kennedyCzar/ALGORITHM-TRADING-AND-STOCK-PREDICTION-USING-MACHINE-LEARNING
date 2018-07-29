0f2551f2fcd7b7ca1199bd8275850c429658e7e6
KennedyWolfie <kennyjunior022006@yahoo.com>
2018-07-29 23:50:07 +0200

    Regression Forecast
    future forecasting

--- Algorithm trading using machine learning/_SCRIPT/CAPSTONE.py
+++ Algorithm trading using machine learning/_SCRIPT/CAPSTONE.py
@@ -509,7 +509,7 @@ def Regression(dataframe, feature, start_date, end_date):
 
 
 #Unit testing
-Regression('IBM', 'Close', datetime(2000, 1, 1), datetime(2018, 7, 16))
+Regression('IBM', 'Close', datetime(1976, 1, 1), datetime(2018, 7, 16))
 
 
 #Data
@@ -518,16 +518,163 @@ Regression('IBM', 'Close', datetime(2000, 1, 1), datetime(2018, 7, 16))
 #ford = web.DataReader('F', 'yahoo', start, end)
 #gm = web.DataReader('GM', 'yahoo', start, end)
 #dax = web.DataReader('DAX', 'yahoo', start, end)
-#if __name__ == '__main__':
+
+
+#%% PREDICT FURTHER YEARS/MONTHS
+
+
+def forecast(dataframe, feature, start_date, end_date, new_end_date):
+    
+    '''
+    Arguments:
+        dataframe: list of dataframe we are loading
+        dataname: This is the required name of the company deta we want to perform
+                    regression
+        feature: It indicates the dependent variable we would be prediction
+        start_date: As implied signifies the start date of the stock we intend to predict
+        end_date:   As implied signifies the end date of the stock we intend to predict
+        
+    '''
+    #Import required libaries
+    
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    from datetime import datetime
+    import pandas_datareader.data as web
+    
+    #-----------------------------
+    #start_date = datetime(1976, 1, 1)
+    #end_date = datetime(2018, 7, 16)
+    #dataframe = ['TSLA', 'IBM', 'AAPL', 'MSFT', 'F', 'GM']
+    
 #    try:
 #        if start_date == None and end_date == None:
 #            raise('No data to plot regression')
 #        else:
-#            call_class = regressioon_plot(dataframe, feature, start_date, end_date)
+#            for ii in dataframe:
+#                data = web.DataReader(dataframe[ii], "yahoo", start_date, end_date)
 #    except ValueError:
 #        raise
 #    finally:
 #        pass
+    data = web.DataReader(dataframe, "yahoo", start_date, end_date)
+    #define the feature vector we would be using for 
+    #to plot our regression
+    df = data[[feature]]
+    
+    #Its a dataframe so we have to convert
+    #it into a numerical data
+    #We also dont need this since the data is already in float
+    #df.info() to check datatype
+    #df['Open'] = pd.to_numeric(df['Open'], errors='coerce')
+    
+    df['Volatility'] = df[feature] - df[feature].shift(1)
+    df = df.dropna()
+    
+    #linear regression model
+    from sklearn.linear_model import LinearRegression
+    
+    #this we would be using to draw our regression line
+    Xf1 = np.arange(1, len(df)+ 1)
+    Xf2 = (Xf1**2).astype(np.float64)
+    Xf3 = (Xf1**3).astype(np.float64)
+    #Xf4 = (Xf1**4).astype(np.float64)
+    
+    #put our numpy array in a list
+    Xf = [Xf1, Xf2, Xf3]#, Xf4]
+    #transpose and reshape our data into (Nx4)Dimensions
+    Xf = np.reshape(Xf, (3, len(df))).T
+    
+    
+    #create a regression class
+    regress = LinearRegression(n_jobs = -1)
+    regress.fit(Xf, df[feature])
+    
+    #get the coefficients and intercept
+    coeffs = regress.coef_
+    intercept = regress.intercept_
+    
+    #create a Regression and residual column
+    #in out dataframe
+    df['Regression'] = intercept + coeffs[0] * Xf1 + coeffs[1] * Xf2 + coeffs[2] * Xf3# + coeffs[3] * Xf4
+    df['Residuals'] = df[feature] - df['Regression'] #Not needed now untill further analysis is required.
+    std_regress = df['Regression'].std()
+    std_open = df[[feature]].std()
+    
+    
+    
+    #plot future price
+    #----------------------------------------------
+    #new_end_date = datetime(2020, 7, 16)
+    dates = pd.date_range(start_date, new_end_date)
+    dt = np.arange(1, len(dates) + 1)
+    dt2 = dt **2
+    dt3 = dt **3
+    #dt4 = dt **4
+    
+    dt_predict = intercept + coeffs[0] * dt + coeffs[1] * dt2 + coeffs[2] * dt3# + coeffs[3] * dt4
+    dt_predict = pd.DataFrame(data=dt_predict, index=dates)
+    #dt_predict.index.name = 'Date'
+    #dt_predict.columns = [[feature]]
+    df['predicted'] = dt_predict
+    df['Upper regresss bound'] = df['predicted'] + (df['Regression'].std())
+    df['Lower regresss bound'] = df['predicted'] - (df['Regression'].std())
+    df['Actual'] = df[[feature]]
+    df[['Actual', 'predicted', 'Upper regresss bound', 'Lower regresss bound']].plot(lw = 1.)
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+    #----------------------------------------------------
+#    actual = df[[feature]]
+#    
+#    plt.plot(actual, label="Actual")
+#    plt.plot(dt_predict, label="Predicted")
+#    plt.plot(dt_predict - std_regress, label='Upper regresss bound')
+#    plt.plot(dt_predict + std_regress, label='Lower regresss bound')
+#    plt.legend(loc='best')
+#    plt.show()
+
+dataframe = ['TSLA', 'IBM', 'AAPL', 'MSFT', 'F', 'GM']
+forecast('IBM', 'Close', datetime(1976, 1, 1), datetime(2018, 7, 16), new_end_date = datetime(2020, 7, 16))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
