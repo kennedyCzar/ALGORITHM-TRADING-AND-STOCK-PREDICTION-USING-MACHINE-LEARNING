d648fb2549b19dacb63d83ac85a91fa427f71beb
KennedyWolfie <kennyjunior022006@yahoo.com>
2018-07-29 23:50:37 +0200

    Update Regression class
    Update Regression class

--- Algorithm trading using machine learning/_SCRIPT/Regression.py
+++ Algorithm trading using machine learning/_SCRIPT/Regression.py
@@ -34,6 +34,9 @@ class Regression():
         self.start_date = start_date
         self.end_date = end_date
         
+        #################################################################
+        #### LOAD DATA
+        #################################################################
         #Load data using datetime for the
         #start and end date
         #start_date = datetime(1976, 1, 1)
@@ -42,16 +45,19 @@ class Regression():
             if self.start_date == None and self.end_date == None:
                 raise('No data to plot regression')
             else:
-                self.dataframe = web.DataReader(self.dataframe, "yahoo", self.start_date, self.end_date)
+                self.data = web.DataReader(self.dataframe, "yahoo", self.start_date, self.end_date)
         except ValueError:
             raise
         finally:
             pass
         
+    #################################################################
+    #### REGRESSION MODULE
+    #################################################################
     def regress(self):
         #define the feature vector we would be using for 
         #to plot our regression
-        df = self.dataframe[[self.feature]]
+        df = self.data[[self.feature]]
         
         #Its a dataframe so we have to convert
         #it into a numerical data
@@ -67,14 +73,14 @@ class Regression():
         
         #this we would be using to draw our regression line
         Xf1 = np.arange(1, len(df)+ 1)
-        #Xf2 = (Xf1**2).astype(np.float64)
-        #Xf3 = (Xf1**3).astype(np.float64)
+        Xf2 = (Xf1**2).astype(np.float64)
+        Xf3 = (Xf1**3).astype(np.float64)
         #Xf4 = (Xf1**4).astype(np.float64)
         
         #put our numpy array in a list
-        Xf = [Xf1]#, Xf2, Xf3, Xf4]
+        Xf = [Xf1, Xf2, Xf3]#, Xf4]
         #transpose and reshape our data into (Nx4)Dimensions
-        Xf = np.reshape(Xf, (1, len(df))).T
+        Xf = np.reshape(Xf, (3, len(df))).T
         
         
         #create a regression class
@@ -87,13 +93,13 @@ class Regression():
         
         #create a Regression and residual column
         #in out dataframe
-        df['Regression'] = intercept + coeffs[0] * Xf1# + coeffs[1] * Xf2 + coeffs[2] * Xf3 + coeffs[3] * Xf4 
+        df['Regression'] = intercept + coeffs[0] * Xf1 + coeffs[1] * Xf2 + coeffs[2] * Xf3# + coeffs[3] * Xf4 
         df['Residuals'] = df[self.feature] - df['Regression']
         df['Upper regresss bound'] = df['Regression'] + df['Regression'].std()
         df['Lower regresss bound'] = df['Regression'] - df['Regression'].std()
         
         #plot 2D chart of result
-        df[[self.feature, 'Regression', 'Upper regresss bound', 'Lower regresss bound',]].plot(title = 'Polynomial Regression line for {}'.format(self.dataframe))
+        df[[self.feature, 'Regression', 'Upper regresss bound', 'Lower regresss bound',]].plot(LW = 1., title = 'Polynomial Regression line for {}'.format(self.dataframe))
         plt.legend(loc = 'best')
         plt.grid(True)
         plt.show()
@@ -104,8 +110,8 @@ class Regression():
 
 
 if __name__ == '__main__':
-    #create an object of the regression class
-    c = Regression('IBM', 'Close', datetime(2014, 1, 1), datetime(2018, 7, 16))
+    #create an object of the regression class | Unit testing
+    c = Regression('GM', 'Open', datetime(1976, 1, 1), datetime(2018, 7, 16))
     #call the attribute of the Regression class
     c.regress()
 
